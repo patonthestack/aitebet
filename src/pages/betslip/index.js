ac707ae4542188242e8bbf31741994733e58d794
Kevin Herrera <1herrerake@gmail.com>
2021-05-11 22:52:24 -0500

    added chakra icons. betslip page prototype

--- src/pages/betslip/index.js
+++ src/pages/betslip/index.js
@@ -12,7 +12,21 @@ import {
 } from '@chakra-ui/react';
 import { Container, Layout } from 'components/index';
 import NextLink from 'next/link';
+import { StarIcon, InfoOutlineIcon, WarningIcon } from '@chakra-ui/icons';
 
+const topFriends = [
+  { name: 'Patrick Tep', picture: '', username: '@ptep03', link: '/' },
+  { name: 'John Grayson', picture: '', username: '@jgray26', link: '/' },
+  { name: 'Dashon Latta', picture: '', username: '@xxxdada89', link: '/' },
+  { name: 'Vu Nguyen', picture: '', username: '@vuvuzela', link: '/' },
+];
+
+const friends = [
+  { name: 'Tony Rojas', picture: '', username: '@skinflutes', link: '/' },
+  { name: 'Daniel Nguyen', picture: '', username: '@dsun', link: '/' },
+  { name: 'Henry Portillo', picture: '', username: '@henryp15', link: '/' },
+  { name: 'Selwin Maz', picture: '', username: '@selmaz', link: '/' },
+];
 const Betslip = () => {
   return (
     <Layout
@@ -23,8 +37,8 @@ const Betslip = () => {
       isActive={true}
       isBanned={false}
     >
-      <Container fluid>
-        <Center>
+      <div>
+        <Center py="10px">
           <Box>
             <Text>Add Recipients</Text>
           </Box>
@@ -32,82 +46,43 @@ const Betslip = () => {
         <box>
           <Input placeholder="Name, @username, phone, or email" />
         </box>
-        <Text textAlign={['center', 'center', 'center', 'center']}>
-          This is the create a betslip page.
-        </Text>
-        <Box bg="gray.300" py="5px">
+        <Box bg="gray.300" py="5px" pl="1em">
           Top People
         </Box>
-        <VStack
-          divider={<StackDivider borderColor="gray.200" />}
-          spacing={4}
-          align="stretch"
-        >
-          <Flex h="40px" bg="yellow.200">
-            <Box>PIC</Box>
-            <Spacer />
-            <Box>
-              <Box>Name</Box>
-              <Box>@Handle</Box>
-            </Box>
-            <Spacer />
-            <Box>i</Box>
-          </Flex>
-          <Flex h="40px" bg="yellow.200">
-            <Box>PIC</Box>
-            <Spacer />
-            <Box>
-              <Box>Name</Box>
-              <Box>@Handle</Box>
-            </Box>
-            <Spacer />
-            <Box>i</Box>
-          </Flex>
-          <Flex h="40px" bg="yellow.200">
-            <Box>PIC</Box>
-            <Spacer />
-            <Box>
-              <Box>Name</Box>
-              <Box>@Handle</Box>
-            </Box>
-            <Spacer />
-            <Box>i</Box>
-          </Flex>
-          <Box bg="gray.300" py="5px">
-            Friends
-          </Box>{' '}
-          <Flex h="40px" bg="yellow.200">
-            <Box>PIC</Box>
-            <Spacer />
-            <Box>
-              <Box>Name</Box>
-              <Box>@Handle</Box>
-            </Box>
-            <Spacer />
-            <Box>i</Box>
-          </Flex>
-          <Flex h="40px" bg="yellow.200">
-            <Box>PIC</Box>
-            <Spacer />
-            <Box>
-              <Box>Name</Box>
-              <Box>@Handle</Box>
-            </Box>
-            <Spacer />
-            <Box>i</Box>
-          </Flex>
-          <Flex h="40px" bg="yellow.200">
-            <Box>PIC</Box>
-            <Spacer />
-            <Box>
-              <Box>Name</Box>
-              <Box>@Handle</Box>
-            </Box>
-            <Spacer />
-            <Box>i</Box>
-          </Flex>
+        <VStack align="stretch">
+          {topFriends.map((item) => {
+            return (
+              <Flex px="1em" alignItems="center" key={item.username}>
+                <StarIcon />
+                <Box ml="1em">
+                  <Box>{item.name}</Box>
+                  <Box fontSize="12px">{item.username}</Box>
+                </Box>
+                <Spacer />
+                <InfoOutlineIcon />
+              </Flex>
+            );
+          })}
         </VStack>
-      </Container>
+        <VStack align="stretch">
+          <Box bg="gray.300" py="5px" pl="1em">
+            Friends
+          </Box>
+          {friends.map((item) => {
+            return (
+              <Flex pr="1em" pl="1em" alignItems="center" key={item.username}>
+                <StarIcon />
+                <Box ml="1em">
+                  <Box>{item.name}</Box>
+                  <Box fontSize="12px">{item.username}</Box>
+                </Box>
+                <Spacer />
+                <InfoOutlineIcon />
+              </Flex>
+            );
+          })}
+        </VStack>
+      </div>
     </Layout>
   );
 };
