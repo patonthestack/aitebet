5be0835d875e84dcd248e8b094848586fdda6ec1
Kevin Herrera <1herrerake@gmail.com>
2021-05-16 15:12:01 -0500

    removed code not in use

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,20 +1,13 @@
-import { Layout, DashboardNav } from 'components/index';
-import React, { useEffect, useState } from 'react';
+import { Layout } from 'components/index';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from 'lib/auth';
 import {
   Box,
-  Button,
   Flex,
-  Heading,
-  Text,
-  Stack,
   SimpleGrid,
-  Avatar,
 } from '@chakra-ui/react';
-import { userProviderCheck } from 'helpers/index';
-import NextLink from 'next/link';
-import { useDocument, useCollection } from '@nandorojo/swr-firestore';
+import { useDocument } from '@nandorojo/swr-firestore';
 import DashboardCard from '@/components/Dashboard/DashboardCard';
 
 const DashboardIndex: React.FC = () => {
@@ -40,59 +33,15 @@ const DashboardIndex: React.FC = () => {
           title="Dashboard"
           description="dashboard"
           canonical="/dashboard"
-          hasNavbar
-          hasFooter
           schemaData={null}
           // isActive={userData?.user._isActive}
           // isBanned={userData?.user._isBanned}
         >
           <Flex>
             <Box
-              w={['0px', '300px', '300px']}
-              position="fixed"
-              display={['none', 'none', 'block']}
-              top="60px"
-              h="100%"
-              bg="blue.300"
-            >
-              <DashboardNav />
-            </Box>
-
-            <Box
               flex="1"
-              ml={['0px', '0px', '300px']}
               display="block"
-              top="60px"
             >
-              <Flex
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-                textAlign="center"
-                mt={20}
-              >
-                {/* <Flex direction="column" align={['center', 'center']} my="20px">
-                  <NextLink href="/avatarupload">
-                    <a>
-                      <Avatar
-                        w={['3rem', '6rem']}
-                        h={['3rem', '6rem']}
-                        mb={4}
-                        src={userData?.user?.photoUrl}
-                      />
-                    </a>
-                  </NextLink>
-                  <Heading>
-                    {userData?.user?.firstName} {userData?.user?.middleName}{' '}
-                    {userData?.user?.lastName}
-                  </Heading>
-                  <Text>{userData?.user?.email}</Text>
-                  {/* <Text>{userData?.user?.userType}</Text> */}
-                {/* <Text>{userRoleCheck(userData)}</Text>
-                </Flex>  */}
-              </Flex>
-
               <SimpleGrid
                 spacing={6}
                 columns={[1, 1, 2]}
